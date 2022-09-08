14732f2135a13737c675c0f9579120a953e05a10
Arsen <mkrtichan2021@gmail.com>
2022-09-08 10:36:46 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+      adaptivHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icon/arrow/left_arrow.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icon/arrow/right_arrow.svg"></button>',
+    });
+  });
